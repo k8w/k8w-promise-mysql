0d5a2dd9c920922ed20f6c48dd4703b8f18a5728
Incep-3 <1425688092@qq.com>
2018-05-21 14:15:28 +0800

    bug fix

--- test/MySQLTest.test.ts
+++ test/MySQLTest.test.ts
@@ -1,40 +1,53 @@
-import MyClusterConfig from '../src/MyClusterConfig';
 import PromiseMySQL from '../index';
-
+import * as assert from 'assert'
 describe('cluster', function () {
     it('cluster', async function () {
-        let ccfg: MyClusterConfig = {
+        let ccfg: any = {
             options: {},
             master: [{
                 acquireTimeout: 10 * 1000,
-                host: '192.168.1.245',
+                host: '10.42.1.42',
                 // host: '127.0.0.1',
                 port: 3306,
                 connectionLimit: 10,
-                user: 'root',
-                password: '',
+                user: 'test',
+                password: 'test',
                 // password: 'miyoo123',
-                database: 'oldquiz',
+                database: 'test'
             }],
             slave: [{
                 acquireTimeout: 10 * 1000,
-                host: '192.168.1.245',
+                host: '10.42.0.31',
                 // host: '127.0.0.1',
                 port: 3306,
                 connectionLimit: 10,
-                user: 'root',
-                password: '',
+                user: 'test',
+                password: 'test',
                 // password: 'miyoo123',
-                database: 'oldquiz',
+                database: 'test'
+            }, {
+                acquireTimeout: 10 * 1000,
+                host: '10.42.1.43',
+                // host: '127.0.0.1',
+                port: 3306,
+                connectionLimit: 10,
+                user: 'test',
+                password: 'test',
+                // password: 'miyoo123',
+                database: 'test'
             }]
         };
         let sql = PromiseMySQL.createCluster(ccfg);
-        var u2 = (await sql.query('SELECT * FROM ai_user ORDER BY RAND() LIMIT 1')).results[0]
 
-        // let pool1 = PromiseMySQL.createPool(ccfg.master[0]);
-        // var user: any = (await pool1.select('SELECT * FROM ai_user ORDER BY RAND() LIMIT 1')).results[0];
-        // console.log(user)
+        let user = { uid: 1111, name: "rur4" };
+        await sql.delete('SELECT * FROM `user` where uid =1111');
+        await sql.insert('INSERT INTO user SET ?', [user]);
+
+        let u2 = (await sql.select('SELECT * FROM `user` where uid =1111')).results[0];
+
         console.log(u2)
+
+        assert.deepEqual(user, u2);
     }
     )
 })
